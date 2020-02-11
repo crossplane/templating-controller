ada9567e67ce4883ccb7bd1c804319ffc5229700
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-02-11 17:47:53 +0300

    Fix YAML decoding. Tested
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- pkg/operations/helm3/helm3.go
+++ pkg/operations/helm3/helm3.go
@@ -18,14 +18,14 @@ package helm3
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
-
-	"gopkg.in/yaml.v3"
 
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/yaml"
 
 	"github.com/crossplaneio/templating-controller/pkg/resource"
 )
@@ -71,11 +71,20 @@ func (h *Engine) Run(cr resource.ParentResource) ([]resource.ChildResource, erro
 	}
 	i := action.NewInstall(&config)
 	i.DryRun = true
-	i.ReleaseName = "dumb"
+	i.ReleaseName = cr.GetName()
 	i.Replace = true // Skip the name check
 	i.ClientOnly = true
 	// i.APIVersions = chartutil.VersionSet{}
-	release, err := i.Run(chart, cr.UnstructuredContent())
+	values := map[string]interface{}{}
+	if valuesMap, exists := cr.UnstructuredContent()["spec"]; exists {
+		if valuesCasted, ok := valuesMap.(map[string]interface{}); !ok {
+			return nil, errors.New("spec could not be parsed into a map")
+		} else {
+			values = valuesCasted
+		}
+	}
+
+	release, err := i.Run(chart, values)
 	if err != nil {
 		return nil, err
 	}
@@ -83,18 +92,18 @@ func (h *Engine) Run(cr resource.ParentResource) ([]resource.ChildResource, erro
 }
 
 func parse(source []byte) ([]resource.ChildResource, error) {
-	dec := yaml.NewDecoder(bytes.NewReader(source))
+	dec := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(source), 4096)
 	var result []resource.ChildResource
 	for {
-		var resMap map[string]interface{}
-		err := dec.Decode(&resMap)
+		u := &unstructured.Unstructured{}
+		err := dec.Decode(u)
 		if err != nil && err != io.EOF {
 			return nil, err
 		}
 		if err == io.EOF {
 			break
 		}
-		result = append(result, &unstructured.Unstructured{Object: resMap})
+		result = append(result, u)
 	}
 	return result, nil
 }
